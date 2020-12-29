1b1b032c99d11c39402c6de71656ceb7d9632433
EC2 Default User <shota6290@gmail.com>
2020-12-29 05:49:54 +0000

    課題提出php_13

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,14 @@ Route::get('/', function () {
 });   
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create'); # 追記
 
 
     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    
     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
 
